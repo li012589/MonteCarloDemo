343557e7f8e4d1873530d1794c616e1ed6dfb95a
Li SH <li012589li@outlook.com>
2017-05-11 15:24:57 +0800

    add create field func, and change settings.txt

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
 from setting import Settings
+import numpy as np
 
+settingPath = './settings.txt'
 
+def createField(size, method):
+    if method != -1:
+        tmp = np.empty(tuple(size))
+        tmp[:] = method
+        return tmp
+    else:
+        tmp = np.random.random(tuple(size))
+        tmp[tmp<0.5] = -1
+        tmp[tmp>=0.5] = 1
+        return tmp
+
+def calculateH(image, b, j):
+    H = 0
+    shape = image.shape
+    for i in range(shape[0]):
+        for j in range(shape[1]):
+            H += -b*image[i,j]
+            if i-1>0 and j-1>0:
+                H += -j*image[i,j]*image[i-1,j-1]
+            if i-1>0 and j+1<shape[1]:
+                H += -j*image[i,j]*image[i-1,j+1]
+            if i+1<shape[0] and j-1>0:
+                H += -j*image[i,j]*image[i+1,j-1]
+            if i+1<shape[0] and j+1<shape[1]:
+                H += -j*image[i,j]*image[i+1,j+1]
+    return H
+
+def calculateDeltaH():
+    pass
 
 def main():
-    pass
+    settings = Settings(settingPath)
+    fieldSize = settings.getValue('size')
+    field = createField(fieldSize,settings.getValue('init'))
+    print field
 
 if __name__ == '__main__':
     main()
