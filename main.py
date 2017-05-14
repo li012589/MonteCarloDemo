1769c3f12229e0152bf0f94579ee56fd476ae40b
Li SH <li012589li@outlook.com>
2017-05-14 23:08:32 +0800

    add woffl method to main

--- main.py
+++ main.py
@@ -5,21 +5,41 @@ from random import randint
 import matplotlib.pyplot as plt
 from general import createField,calculateH,calculateM
 from Metropolis import Metropolis
+from Woffl import Woffl
 
 generalSettingPath = './general_settings.txt'
 MetroSettingPath = './Metropolis_settings.txt'
 WofflSettingPath = './Woffl_settings.txt'
 
 def main():
+
+    print "using Metropolis method"
     m = Metropolis(MetroSettingPath)
-    f = createField(m.fieldSize,m.fieldInitMethod)
+    fm = createField(m.fieldSize,m.fieldInitMethod)
 #    plt.matshow(f)
-    m.init(f)
-    initH = calculateH(m.field,m.Hami[0],m.Hami[1])
-    initM = calculateM(m.field)
-    m.run(m.maxIter)
+    m.init(fm)
+    initHm = calculateH(m.field,m.Hami[0],m.Hami[1])
+    initMm = calculateM(m.field)
+    #m.run(m.maxIter)
     #plt.plot(m.HHistory(initH))
-    plt.plot(m.MHistory(initM))
+    plt.figure()
+    plt.plot(m.MHistory(initMm))
+    plt.ylabel('M')
+    plt.xlabel('iterations')
+    plt.title('Metropolis')
+
+    print "using Woffl method"
+    w = Woffl(WofflSettingPath)
+    fw = createField(w.fieldSize,w.fieldInitMethod)
+    w.init(fw)
+    initHw = calculateH(w.field,w.Hami[0],w.Hami[1])
+    initMw = calculateM(w.field)
+    w.run(w.maxIter)
+    plt.figure()
+    plt.plot(w.MHistory(initMm))
+    plt.ylabel('M')
+    plt.xlabel('iterations')
+    plt.title('Woffl')
     plt.show()
 
 
